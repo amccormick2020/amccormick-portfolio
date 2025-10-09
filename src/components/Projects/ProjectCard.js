521dec0e04df95d29212231e3894fefb9c4b70e1
Austin McCormick <amccormick2020@tamu.edu>
2025-10-09 00:07:08 -0500

    Added project card template

---
+++ src/components/Projects/ProjectCard.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { Container, Row, Col, Card, Badge } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { CgWebsite } from "react-icons/cg";
+import { BsGithub } from "react-icons/bs";
+
+function Projectcard(props) {
+  return (
+    <Card className="card-view">
+
+      {/* Project Image */}
+      <Card.Img src={props.imgPath} alt="card-img" />
+      <Card.Body>
+
+        {/* Project Title */}
+        <Card.Title style={{ fontSize: "1.5rem", fontWeight: "bold" }}>{props.title}</Card.Title>
+
+        {/* Project Tags */}
+        <div style={{ margin: "1.5vh" }}>
+            {props.tags?.map((tag, index) => (
+              <Badge
+                key={index}
+                pill
+                bg="dark"
+                style={{
+                  marginRight: "0.5vh",
+                  marginBottom: "0.5vh",
+                  fontSize: "0.9rem",
+                  color: "#c770f0"
+                }}
+              >
+                {tag}
+              </Badge>
+              ))}
+            </div>
+
+        {/* Project Description */}
+        <Card.Text className="r" style={{ textAlign: "justify" }}>
+          {props.description}
+        </Card.Text>
+
+        <Row>
+        {/* Project Timestamp */}
+        <Col className="d-flex align-items-center">
+          <span className="roboto-chars purple" style={{ fontSize: "1.2rem" }}>
+            {props.timestamp}
+          </span>
+        </Col>
+        {/* Project Links */}
+        <Col>
+        <Button variant="primary" href={props.ghLink} target="_blank">
+          <BsGithub /> &nbsp;
+          {"GitHub"}
+        </Button>
+        {"\n"}
+        {"\n"}
+
+        {props.demoLink && (
+          <Button
+            variant="primary"
+            href={props.demoLink}
+            target="_blank"
+            
+          >
+            <CgWebsite /> &nbsp;
+            {"Demo"}
+          </Button>
+        )}
+        </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
+}
+export default Projectcard;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
+import ProjectCard from "./ProjectCard";
 import {AiFillGithub} from "react-icons/ai";
+import portfolio from "../../Assets/projects/thumbnail.png";
+
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -26,7 +29,20 @@ function Projects() {
                           <AiFillGithub />
                         </a>
 
-                  </Col>
+          </Col>
+        </Row>
+
+        <Row style={{ justifyContent: "center", paddingBottom: "1vh" }}>
+          <Col md="4">
+          <ProjectCard
+            timestamp="October 2025"
+            imgPath={portfolio}
+            title="React-Based Portfolio"
+            description="This is the website you are currently visiting! I wanted to create an interactive and consolidated way to showcase my qualifications and works. I developed this portfolio using React, and it is deployed on an AWS EC2 instance."
+            ghLink="https://github.com/amccormick2020/amccormick-portfolio"
+            tags={["React.js", "Node.js", "JavaScript", "Web Development", "AWS"]}
+          />
+          </Col>
         </Row>
       </Container>
     </Container>
